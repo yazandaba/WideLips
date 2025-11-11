b91568e9c2a7aff564a63d9c2afa702fb9393c20
yazan daba <yazandaba@outlook.com>
2025-11-12 00:26:32 +0400

    WideLips customizable and default LISP parser

---
+++ include/LispParser.h
@@ -0,0 +1,51 @@
+﻿#ifndef PARSER_H
+#define PARSER_H
+#include <memory_resource>
+#include "LispLexer.h"
+
+namespace WideLips {
+    struct LispParseError;
+    struct LispAuxiliary;
+    struct LispAtom;
+
+    class LispParser {
+        friend struct LispParseNodeBase;
+        template<typename T>
+        friend struct LispParseNode;
+        friend struct LispList;
+        friend class LispParseTree;
+    private:
+        AlignedFileReadResult _optionalAlignedFile;
+        std::unique_ptr<LispLexer> _lexer;
+        std::pmr::monotonic_buffer_resource _parseNodesPool;
+        std::pmr::polymorphic_allocator<> _parseNodesAllocator;
+        LispAtom* _endOfProgram;
+    public:
+        WL_API LispParser(const std::filesystem::path &filePath,bool conservative);
+        WL_API LispParser(std::string_view program,bool conservative);
+        LispParser(const LispParser&) = delete;
+        LispParser(LispParser&& ) = delete;
+        LispParser& operator=(const LispParser&) = delete;
+        LispParser& operator=(LispParser&& ) = delete;
+        WL_API virtual ~LispParser() = default;
+    public:
+        NODISCARD WL_API LispParseNodeBase* Parse();
+        NODISCARD WL_API virtual LispParseNodeBase* Parse(const LispToken* sexprBegin,const LispToken* sexprEnd);
+        NODISCARD WL_API const BumpVector<Diagnostic::LispDiagnostic>& GetDiagnostics() const;
+        NODISCARD WL_API std::wstring_view OriginFile() const;
+        WL_API void Reuse() const;
+    protected:
+        NODISCARD virtual LispParseNodeBase* ParseDialectSpecial(const LispToken* currentToken);
+        NODISCARD LispLexer* GetLexer() const;
+    private:
+        WL_HIDDEN NODISCARD LispParseError* OnUnrecognizedToken(const LispToken* currentToken);
+        WL_HIDDEN NODISCARD BumpVector<Diagnostic::LispDiagnostic>& GetDiagnosticsInternal() const;
+        NODISCARD LispAuxiliary* MakeAuxiliary(const LispToken* auxBegin,const LispToken* auxEnd);
+        NODISCARD LispList* MakeList(const LispToken* sexprBegin,const LispToken* sexprEnd);
+        NODISCARD LispAtom * MakeEndOfProgram() const;
+    };
+}
+
+
+
+#endif //PARSER_H
---
+++ src/LispParser.cpp
@@ -0,0 +1,152 @@
+﻿#include "LispParseTree.h"
+#include "LispParser.h"
+
+namespace WideLips {
+    LispParser::LispParser(const std::string_view program, const bool conservative):
+    _optionalAlignedFile(nullptr),
+    _lexer(LispLexer::Make(program,conservative)),
+    _parseNodesPool(ArenaSizeEstimate(program.size()/2,conservative)),
+    _parseNodesAllocator(&_parseNodesPool),
+    _endOfProgram(_parseNodesAllocator.new_object<LispAtom>(&PredefinedTokens::EndOfFile,
+                LispParseNodeKind::EndOfProgram,
+                nullptr,
+                nullptr,
+                this
+            )){
+    }
+
+    LispParser::LispParser(const std::filesystem::path &filePath, const bool conservative):
+    _optionalAlignedFile(AlignedFileReader::Read(filePath)),
+    _lexer(LispLexer::Make(_optionalAlignedFile,filePath.native(),conservative)),
+    _parseNodesPool(ArenaSizeEstimate(_lexer->GetFileSize(),conservative)),
+    _parseNodesAllocator(&_parseNodesPool),
+    _endOfProgram(_parseNodesAllocator.new_object<LispAtom>(&PredefinedTokens::EndOfFile,
+                LispParseNodeKind::EndOfProgram,
+                nullptr,
+                nullptr,
+                this
+            )){
+    }
+
+    LispParseNodeBase* LispParser::Parse() {
+        _lexer->Tokenize();
+        const auto optRegion = _lexer->TokenizeFirstSExpr();
+        if (!optRegion) {
+            return nullptr;
+        }
+        const auto& [sexprBegin,sexprEnd] = *optRegion;
+        return _parseNodesAllocator.new_object<LispList>(
+                    sexprBegin,
+                    sexprEnd,
+                    nullptr,
+                    nullptr,
+                    nullptr,
+                    this
+                );
+    }
+
+    LispParseNodeBase* LispParser::Parse(const LispToken* sexprBegin,const LispToken* sexprEnd) {
+        LispParseNodeBase* subExpressionsHead = nullptr;
+        for (auto currentToken=sexprEnd; currentToken>=sexprBegin; --currentToken) [[likely]]{
+            if (currentToken->Match(LispTokenKind::RightParenthesis)) {
+                auto openParen = currentToken - 1; //skip to opening parenthesis
+                subExpressionsHead =  _parseNodesAllocator.new_object<LispList>(
+                    openParen,
+                    currentToken--,
+                    nullptr,
+                    subExpressionsHead,
+                    nullptr,
+                    this
+                );
+            }
+            else if (currentToken->IsOperator()) {
+                subExpressionsHead = _parseNodesAllocator.new_object<LispAtom>(currentToken,
+                     LispParseNodeKind::Operator,
+                     subExpressionsHead,
+                     nullptr,
+                     this
+                );
+            }
+            else if (currentToken->IsDialectSpecial()) [[unlikely]]{
+                LispParseNodeBase* const dsNode = ParseDialectSpecial(currentToken);
+                dsNode->Next = subExpressionsHead;
+                subExpressionsHead = dsNode;
+            }
+            else if (currentToken->Match(LispTokenKind::Invalid)) [[unlikely]] {
+                subExpressionsHead = _parseNodesAllocator.new_object<LispParseError>(
+                    currentToken,
+                    subExpressionsHead,
+                    nullptr,
+                    this
+                );
+            }
+            else [[likely]]{
+                subExpressionsHead = _parseNodesAllocator.new_object<LispAtom>(currentToken,
+                    static_cast<LispParseNodeKind>(currentToken->Kind),
+                    subExpressionsHead,
+                    nullptr,
+                    this
+                );
+            }
+        }
+
+        return subExpressionsHead;
+    }
+
+    std::wstring_view LispParser::OriginFile() const {
+        return _lexer->GetFilePath();
+    }
+
+    LispLexer * LispParser::GetLexer() const {
+        return _lexer.get();
+    }
+
+    LispAuxiliary * LispParser::MakeAuxiliary(const LispToken *auxBegin, const LispToken *auxEnd) {
+        return _parseNodesAllocator.new_object<LispAuxiliary>(std::pair{auxBegin,auxEnd});
+    }
+
+    LispList * LispParser::MakeList(const LispToken *sexprBegin, const LispToken *sexprEnd) {
+        return _parseNodesAllocator.new_object<LispList>(sexprBegin,sexprEnd,nullptr,nullptr,nullptr,this);
+    }
+
+    LispAtom * LispParser::MakeEndOfProgram() const {
+        return _endOfProgram;
+    }
+
+    void LispParser::Reuse() const {
+        _lexer->Reuse();
+    }
+
+    const BumpVector<Diagnostic::LispDiagnostic> &LispParser::GetDiagnostics() const {
+        return _lexer->GetDiagnostics();
+    }
+
+    LispParseNodeBase* LispParser::ParseDialectSpecial(const LispToken *currentToken) {
+        if (currentToken->Match(LispTokenKind::QuasiColumn) ||
+            currentToken->Match(LispTokenKind::Comma) ||
+            currentToken->Match(LispTokenKind::At)) {
+            return _parseNodesAllocator.new_object<LispAtom>(currentToken,
+                 LispParseNodeKind::Operator,
+                 nullptr,
+                 nullptr,
+                 this);
+        }
+
+        return OnUnrecognizedToken(currentToken);
+    }
+
+    LispParseError* LispParser::OnUnrecognizedToken(const LispToken *currentToken) {
+        GetDiagnosticsInternal().EmplaceBack(Diagnostic::DiagnosticFactory::UnrecognizedToken(
+             OriginFile(),
+             currentToken->Line,
+             currentToken->Column,
+             *currentToken));
+
+        return _parseNodesAllocator.new_object<LispParseError>(currentToken,nullptr,nullptr,this);
+    }
+
+    BumpVector<Diagnostic::LispDiagnostic>& LispParser::GetDiagnosticsInternal() const {
+        return _lexer->GetDiagnostics();
+    }
+
+}
