0a4c12fcbeb8991ea4b65ab36b726536fe2aa983
yazan daba <yazandaba@outlook.com>
2025-11-13 19:38:06 +0400

    Add default parser and walker usage example

---
+++ examples/DummyPrinter.h
@@ -0,0 +1,37 @@
+﻿#ifndef WIDELIPS_DUMMYPRINTER_H
+#define WIDELIPS_DUMMYPRINTER_H
+#include "LispParseTreeVisitor.h"
+
+namespace WideLips::Examples {
+
+    class DummyPrinter: public ImmutableLispParseTreeWalker<DummyPrinter> {
+    public:
+        void Visit(const LispAtom * const atom) const noexcept{
+            if (atom->Kind == LispParseNodeKind::EndOfProgram) {
+                return;
+            }
+            std::puts("Atom: ");
+            std::puts(atom->GetParseNodeText().data());
+            std::puts("\n");
+        }
+
+        void Visit(const LispList * const list) const noexcept{
+            std::puts("List: ");
+            std::puts(list->GetParseNodeText().data());
+            std::puts("\n");
+        }
+
+        void Visit(const LispArguments* const arguments) const noexcept{
+            std::puts("ArgsOrVec: ");
+            std::puts(arguments->GetParseNodeText().data());
+            std::puts("\n");
+        }
+
+        void Visit(const LispParseError * const error) const noexcept{
+            std::puts("Error: ");
+            std::puts(error->GetParseNodeText().data());
+            std::puts("\n");
+        }
+    };
+}
+#endif //WIDELIPS_DUMMYPRINTER_H
---
+++ examples/default/Parse.cpp
@@ -0,0 +1,24 @@
+﻿#include "LispParseTree.h"
+#include "../DummyPrinter.h"
+
+int main() {
+    using namespace std::literals::string_literals;
+    std::string_view schemeProgram = R"(
+        (define (factorial n)
+          (if (<= n 1)
+              1
+              (* n (factorial (- n 1)))))
+
+        (define lst '(1 2 3 4 5))
+
+        (define template `(a b ,lst ,@lst))
+    )";
+
+    auto paddedCore = WideLips::LispParseTree::MakeParserFriendlyString(schemeProgram);
+    auto [result,parseTree]= WideLips::LispParseTree::Parse(std::move(paddedCore), false);
+    auto printer = WideLips::Examples::DummyPrinter{};
+    if (result) {
+        parseTree->Accept(&printer);
+    }
+}
+
